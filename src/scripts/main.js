bd069751be90b5a8fed7195a147333ada9ebb1f9
Angelina Julia de França Mendonça de Melo <angelina.melo@dbserver.com.br>
2024-10-07 18:43:59 -0300

    feat: Adiciona a validação e submit de formulário de login

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,8 @@ const messageErrorInputEmail = document.querySelector(".error-email");
 const password = document.getElementById("senha");
 const messageErrorInputPassword = document.querySelector(".error-password");
 
+const formLogin = document.getElementById("login-form");
+
 function validateEmail(email) {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,}$/;
     return regex.test(email);
@@ -15,6 +17,7 @@ function validatePassoword(password) {
 }
 
 function validarFilds(e){
+    console.log(email.value)
     fildTyped = e.target.value;
     
     isFormated = e.target.name == "email" ? validateEmail(fildTyped) : validatePassoword(fildTyped);
@@ -35,9 +38,22 @@ function validarFilds(e){
     
 }
 
+
 email.addEventListener("blur", validarFilds)
 password.addEventListener("blur", validarFilds)
 
+formLogin.addEventListener("submit", e => {
+    
+    e.preventDefault()
+    validEmail = validateEmail(email.value)
+    validPassword = validatePassoword(password.value)
+
+    if(validEmail && validPassword){
+        alert("Email ou senha incorretas")
+        email.value = "";
+        password.value = "";
+    }
+})
 
 
 
